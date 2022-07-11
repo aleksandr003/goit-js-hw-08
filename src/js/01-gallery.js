8577c68d64b0e78a5d6f9902e7bd5221efadd855
aleksandr003 <herasymenko.oleks@gmail.com>
2022-07-11 21:25:27 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryBlockEl = document.querySelector('.gallery');
+
+// Функція для створення карточки makeGalleryCard(cardInfo)
+
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+  return `
+    <div class="gallery__item">
+        <a href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" />
+        </a>
+    </div>
+  `;
+};
+
+// Створення масиву рядків із елементами
+const galleryCardsArr = galleryItems.map(el => {
+  return makeGalleryCard(el);
+});
+
+// Вставка елементів на сторінку
+galleryBlockEl.insertAdjacentHTML('afterbegin', galleryCardsArr.join(''));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
