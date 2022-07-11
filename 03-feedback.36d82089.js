2fcf286f449db6a2cd72133c152af09e17b7aa60
aleksandr003 <104678398+aleksandr003@users.noreply.github.com>
2022-07-11 21:50:43 +0000

    Deploying to gh-pages from @ aleksandr003/goit-js-hw-08@705698b753c987eb838f1dcbdea8c9ea01d357ac 🚀

--- 03-feedback.36d82089.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form");const e=document.querySelector("input"),t=document.querySelector("textarea"),n=(document.querySelector("button"),function(){return console.log(e.value+" "+t.value)});e.addEventListener("input",n),t.addEventListener("input",n);
-//# sourceMappingURL=03-feedback.36d82089.js.map
--- 03-feedback.cf953478.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".feedback-form");var e=document.querySelector("input"),t=document.querySelector("textarea"),n=(document.querySelector("button"),function(){return console.log(e.value+" "+t.value)});e.addEventListener("input",n),t.addEventListener("input",n)}();
-//# sourceMappingURL=03-feedback.cf953478.js.map
