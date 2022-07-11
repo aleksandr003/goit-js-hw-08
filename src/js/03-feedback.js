f5eb84cd7f021e818f5a4fc435e4afed2eac714b
aleksandr003 <herasymenko.oleks@gmail.com>
2022-07-12 00:29:25 +0300

    2, 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const formEl = document.querySelector('.feedback-form');
+const inputEmailEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+const buttonEl = document.querySelector('button');
 
+const saveInfoInput = function () {
+  return console.log(inputEmailEl.value + ' ' + textareaEl.value);
+};
+
+inputEmailEl.addEventListener('input', saveInfoInput);
+textareaEl.addEventListener('input', saveInfoInput);
