7fac5c1e5aafd22689462f6e9848c344ad4c7b84
aleksandr003 <herasymenko.oleks@gmail.com>
2022-07-12 16:24:55 +0300

    make 2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,10 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
+const onPlay = function ({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
-}
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
